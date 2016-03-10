d78eb7d0e3c9c75a937b2d985a7486567de42902
Matthew <meneal@gmail.com>
2016-03-09 22:46:43 -0500

    added procfile and static index page

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ require('./models')(wagner);
 
 var app = express();
 
+app.get('/', function(request,response){
+	response.sendFile(__dirname + '/index.html')
+})
+
+app.set('port', (process.env.PORT || 3000));
+
 app.use('/api/v1', require('./api')(wagner, chance));
 
-app.listen(3000);
-console.log('Listening on port 3000!');
+app.listen(app.get('port'), function() {
+	console.log('Listening on port ' + app.get('port'));
+});
+
