438ffd87f3369aacd5d78937993b95a9efad2818
Matthew <meneal@gmail.com>
2016-03-25 14:01:44 -0400

    Added in schema changes

--- schema.js
+++ schema.js
@@ -35,33 +35,107 @@ module.exports = new mongoose.Schema({
 		min: 1,
 		max: 100
 	},
+	international: {
+		type: Boolean,
+		required: true,
+	},
 	q1: {
 		type: Number,
-		required: true,
 		min: 1,
 		max: 10
 	},
 	q2: {
 		type: Number,
-		required: true,
 		min: 1,
 		max: 10
 	},
 	q3: {
 		type: Number,
-		required: true,
 		min: 1,
 		max: 10
 	},
 	q4: {
 		type: Number,
-		required: true,
 		min: 1,
 		max: 10
 	},
 	q5: {
 		type: Number,
-		required: true,
+		min: 1,
+		max: 10
+	},
+	q6: {
+		type: Number,
+		min: 1,
+		max: 10
+	},
+	q7: {
+		type: Number,
+		min: 1,
+		max: 10
+	},
+	q8: {
+		type: Number,
+		min: 1,
+		max: 10
+	},
+	q9: {
+		type: Number,
+		min: 1,
+		max: 10
+	},
+	q10: {
+		type: Number,
+		min: 1,
+		max: 10
+	},
+	q11: {
+		type: Number,
+		min: 1,
+		max: 10
+	},
+	q12: {
+		type: Number,
+		min: 1,
+		max: 10
+	},
+	q13: {
+		type: Number,
+		min: 1,
+		max: 10
+	},
+	q14: {
+		type: Number,
+		min: 1,
+		max: 10
+	},
+	q15: {
+		type: Number,
+		min: 1,
+		max: 10
+	},
+	q16: {
+		type: Number,
+		min: 1,
+		max: 10
+	},
+	q17: {
+		type: Number,
+		min: 1,
+		max: 10
+	},
+	q18: {
+		type: Number,
+		min: 1,
+		max: 10
+	},
+	q19: {
+		type: Number,
+		min: 1,
+		max: 10
+	},
+	q20: {
+		type: Number,
 		min: 1,
 		max: 10
 	}
