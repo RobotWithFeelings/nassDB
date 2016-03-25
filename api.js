2de3aaa9720c3f8e18e7c9746d56baac5bb8ba87
Matthew <meneal@gmail.com>
2016-03-25 16:10:55 -0400

    added fix

--- api.js
+++ api.js
@@ -5,9 +5,9 @@ var bodyparser = require('body-parser');
 module.exports = function(wagner,chance,stormpath){
 	var api = express.Router();
 
-  api.use(bodyparser.json());
+  // api.use(bodyparser.json());
 
-  api.use(function(req, res, next) {
+  api.use(bodyparser.json(), function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
