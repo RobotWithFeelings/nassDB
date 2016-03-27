22de26c1580d0578d057b8a1738fd5db72d8c0da
Matthew <meneal@gmail.com>
2016-03-27 16:22:38 -0400

    Still working on a fix for cors issue

--- api.js
+++ api.js
@@ -8,9 +8,15 @@ module.exports = function(wagner,chance,stormpath){
   // api.use(bodyparser.json());
 
   api.use(bodyparser.json(), function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "Content-Type, Accept, X-Requested-With, Session");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+    if ('OPTIONS' == req.method) {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+      res.send(200);
+    }
+    else {
+      next();
+    }
   });
 
   //Route to return survey by name
