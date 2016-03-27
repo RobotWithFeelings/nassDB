17c91e72caf5dc813279d7712a74c6c0b0707f7e
Matthew <meneal@gmail.com>
2016-03-27 16:50:03 -0400

    more changes for cors

--- api.js
+++ api.js
@@ -8,15 +8,11 @@ module.exports = function(wagner,chance,stormpath){
   // api.use(bodyparser.json());
 
   api.use(bodyparser.json(), function(req, res, next) {
-    if ('OPTIONS' == req.method) {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-      res.send(200);
-    }
-    else {
-      next();
-    }
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.send(200);
+    next();
   });
 
   //Route to return survey by name
