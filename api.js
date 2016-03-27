ae571056bc2ca17fefa1dae1d41f427fdd7626dc
Matthew <meneal@gmail.com>
2016-03-27 17:04:47 -0400

    fixing dumb mistake

--- api.js
+++ api.js
@@ -11,7 +11,7 @@ module.exports = function(wagner,chance,stormpath){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    res.sendStatus(200);
+    next();
   });
 
   //Route to return survey by name
