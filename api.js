13abca65cd15d45f470fe3bbe67f74f8f79d5ef0
Matthew <meneal@gmail.com>
2016-03-27 17:25:26 -0400

    changed back to options requirement

--- api.js
+++ api.js
@@ -8,10 +8,15 @@ module.exports = function(wagner,chance,stormpath){
   // api.use(bodyparser.json());
 
   api.use(bodyparser.json(), function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    next();
+    if ('OPTIONS' == req.method) {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+      res.sendStatus(200);
+    }
+    else {
+      next();
+    }
   });
 
   //Route to return survey by name
